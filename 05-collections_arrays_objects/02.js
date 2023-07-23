ae38acc1b9e51e4861bef04bc3e56aaeb4dee575
Nathan Lively <60303904+LiveNathan@users.noreply.github.com>
2023-07-23 11:22:14 -0500

    Refactor code, add conversion and average calculation functions
    Refactored variable name for clarity on its unit of measurement, changing 'lengths' to 'lengthsCentimeters'. Added a function to convert centimeters to inches, 'centimetersToInches', populating a new list 'lengthsInches'. Included a function to calculate the average length 'averageLengths'. This allows reusable conversions and calculations, enhancing the code's readability and maintainability.

--- 05-collections_arrays_objects/02.js
+++ 05-collections_arrays_objects/02.js
@@ -1,5 +1,5 @@
 // This array is made up of lengths in Centimeters
-lengths = [
+lengthsCentimeters = [
     231, 432, 342, 543, 678, 756, 543, 654, 109, 239, 349,
     654, 323, 573, 432, 345, 549, 943, 999, 111, 100, 934,
     934, 342, 543, 442, 726, 432, 123, 657, 432, 654, 435
@@ -7,8 +7,19 @@ lengths = [
 
 /**
  * 1. Use a for loop and one of the functions you wrote in previous labs to
- *    create a new list of the lengths in inches.
- * 2. Use another for loop to log the average length in cm and inches. Log the
+ *    create a new list of the lengths in inches. */
+
+function centimetersToInches(lengths) {
+    let inches = [];
+    for (let i = 0; i < lengths.length; i++) {
+        inches.push(lengths[i] / 2.54);
+    }
+    return inches;
+}
+let lengthsInches = centimetersToInches(lengthsCentimeters);
+
+
+ /* 2. Use another for loop to log the average length in cm and inches. Log the
  *    result with string interpolation. For example, "The average length in cm
  *    is ___ , which is ___ in inches"
  *
@@ -16,5 +27,14 @@ lengths = [
  * paste the code you wrote previously into this file.
  */
 
+function averageLengths(lengths) {
+    let average = 0;
+    for (let i = 0; i < lengths.length; i++) {
+        average += lengths[i];
+    }
+    average = average / lengths.length;
+    return average;
+}
+console.log("The average length in cm is " + averageLengths(lengthsCentimeters) + " , which is " + averageLengths(lengthsInches) + " in inches");
 
 
