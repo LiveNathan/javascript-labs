110412b959efda443b05ae04213497aeadd38f19
Nathan Lively <60303904+LiveNathan@users.noreply.github.com>
2023-07-30 09:05:50 -0500

    Add adjustable text size feature
    A new adjustable text size feature has been added to the sidebar of the portfolio project. This feature will let the user increase or decrease the size of the text on the site for a more personalized reading experience. Two new buttons have been added, '-' and '+', which decrease and increase the text size respectively. Also, the portfolio's CSS file has been updated to accommodate this feature. Furthermore, a few changes in the existing code were made to enhance code readability.

--- 07-js_html_css_dom/portfolio/index.js
+++ 07-js_html_css_dom/portfolio/index.js
@@ -4,8 +4,8 @@ function darkMode() {
     let body = document.body;
     let  footer = document.querySelector('#footer');
     let darkModeButton = document.querySelector('#dark-mode-button');
-
     if (body.classList.contains('dark-mode')) {
+
         body.classList.remove('dark-mode')
         footer.classList.remove('dark-mode');
         darkModeButton.innerText = 'dark mode';
@@ -14,4 +14,23 @@ function darkMode() {
         footer.classList.add('dark-mode');
         darkModeButton.innerText = 'light mode';
     }
+}
+
+document.querySelector('#text-size-smaller-button').addEventListener('click', decreaseBaseTextSize)
+document.querySelector('#text-size-bigger-button').addEventListener('click', increaseBaseTextSize)
+
+function decreaseBaseTextSize() {
+    let textSize = document.querySelector('#text-size');
+    textSize.innerText = parseInt(textSize.innerText) - 1;
+    let currentFontSize = window.getComputedStyle(document.body).fontSize;
+    let newFontSize = parseInt(currentFontSize) - 1;
+    document.body.style.fontSize = newFontSize + 'px';
+}
+
+function increaseBaseTextSize() {
+    let textSize = document.querySelector('#text-size');
+    textSize.innerText = parseInt(textSize.innerText) + 1;
+    let currentFontSize = window.getComputedStyle(document.body).fontSize;
+    let newFontSize = parseInt(currentFontSize) + 1;
+    document.body.style.fontSize = newFontSize + 'px';
 }
